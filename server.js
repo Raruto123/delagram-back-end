cbcbdbc06962d3f0d66d7df102e5fc7d392e783f
sanem9206@gmail.com <mohamedsane@192.168.1.5>
2023-07-22 21:46:58 +0000

    j'ai enlevé le _id de res.locals.user

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.use(cors(corsOptions));
 //jwt
 app.get("*", checkUser);
 app.get("/jwtid", requireAuth, (req, res) => {
-    res.status(200).send(res.locals.user._id)
+    res.status(200).send(res.locals.user)
 });
 app.get("/", (req, res) => {
     res.setHeader("Access-Control-Allow-Credentials", "true");
