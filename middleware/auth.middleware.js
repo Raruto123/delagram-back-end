aa1ab60fdedf95d7260d8c05cd06ca822523e582
sanem9206@gmail.com <mohamedsane@192.168.1.5>
2023-07-22 21:38:02 +0000

    middleware modifié

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -11,7 +11,7 @@ module.exports.checkUser = (req, res, next) => {
                 // res.cookie("jwt", "", {maxAge : 1});
                 next();
             }else{
-                let user = await UserModel.findById({_id : decodedToken._id});
+                let user = await UserModel.findById(decodedToken._id);
                 res.locals.user = user;
                 // next();
             }
@@ -54,7 +54,7 @@ module.exports.requireAuth = (req, res, next) => {
                 res.status(401).json({ error: "Invalid or expired token" });
             }else {
                 console.log(decodedToken._id);
-                let user = await UserModel.findById({_id : decodedToken._id});
+                let user = await UserModel.findById(decodedToken._id);
                 res.locals.user = user;
                 // Le token est valide, vous pouvez accéder aux informations utilisateur si nécessaire
                 // Exemple : res.locals.user = await UserModel.findById(decodedToken.id);
